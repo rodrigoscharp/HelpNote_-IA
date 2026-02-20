4f8a5ac1b14e4ece42ed3c33a0fe6a270c0c12a7
Rodrigo Scharp <rodrigosharp99@gmail.com>
2026-02-20 13:44:01 -0300

    configurando o Postgre

--- src/src/main/java/com/example/HelpNote/domain/Note.java
+++ src/src/main/java/com/example/HelpNote/domain/Note.java
@@ -1,10 +1,11 @@
 package com.example.HelpNote.domain;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 
 @Entity
 public class Note {
